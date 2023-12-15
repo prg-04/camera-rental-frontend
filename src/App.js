3878fde76178892d1c0080d7b0afe070eaa6eb78
prg-04 <karanjaevans95@gmail.com>
2023-12-15 16:10:10 +0300

    Fix bug

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import './App.css';
 import { Typography, Container } from '@mui/joy';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 function App() {
   return (
-    <Container className="App">
-
-      {/* We decided to use Mui material for our frontend framework  */}
-      {/* Its easy to use as demonstrated in this file, just
+    <Provider store={store}>
+      <Container className="App">
+        {/* We decided to use Mui material for our frontend framework  */}
+        {/* Its easy to use as demonstrated in this file, just
       import the components you need to use and wrap the content inside the components
       just as you would in html or bootstrap components  */}
-      {/* To change the style or adjust the styles you can use
+        {/* To change the style or adjust the styles you can use
       eg: sx={{display: "inline-flex", justifyContent:"center"}}  */}
-      {/* Link to the MUI/Joy = "https://mui.com/joy-ui/getting-started/installation/" */}
-      {/* For text, Mui uses Typography which we use in all text h1-h6 and paragraph  */}
+        {/* Link to the MUI/Joy = "https://mui.com/joy-ui/getting-started/installation/" */}
+        {/* For text, Mui uses Typography which we use in all text h1-h6 and paragraph  */}
 
-      {/* Typography example please refer to the doc for more options */}
-      <Typography level="h1" sx={{ color: 'red' }}>Hello</Typography>
-
-    </Container>
+        {/* Typography example please refer to the doc for more options */}
+        <Typography level="h1" sx={{ color: 'red' }}>
+          Hello
+        </Typography>
+      </Container>
+    </Provider>
   );
 }
 
